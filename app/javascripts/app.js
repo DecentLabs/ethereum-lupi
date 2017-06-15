92e1c778a958b54246fb3c564e56c935cc6f10cd
Peter Petrovics <szerintedmi@gmail.com>
2017-06-16 00:16:59 +0100

    half baked state of frontend

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,14 +6,12 @@ import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+import lupi_artifacts from '../../build/contracts/Lupi.json'
+import { default as secureRandom} from "secure-random/lib/secure-random.js";
+var moment = require('moment');
+var countdown = require('countdown');
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts);
-
-// The following code is simple to show off interacting with your contracts.
-// As your needs grow you will likely need to change its form and structure.
-// For application bootstrapping, check out window.addEventListener below.
+var Lupi = contract(lupi_artifacts);
 var accounts;
 var account;
 
@@ -21,8 +19,7 @@ window.App = {
   start: function() {
     var self = this;
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider);
+    Lupi.setProvider(web3.currentProvider);
 
     // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function(err, accs) {
@@ -51,39 +48,107 @@ window.App = {
   refreshBalance: function() {
     var self = this;
 
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account, {from: account});
+    var instance;
+    Lupi.deployed().then(function(res) {
+      instance = res;
+      return web3.eth.getBalance(account);
     }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
+        var balance_element = document.getElementById("balance");
+        balance_element.innerHTML = web3.fromWei(value).valueOf();
+        document.getElementById("account").innerHTML = account;
+
+        return instance.getRoundInfo();
+    }).then( roundRes => {
+        var roundInfo = self.parseRoundInfo(roundRes);
+        document.getElementById("roundInfo").innerHTML = JSON.stringify(roundInfo,null, 4);
     }).catch(function(e) {
       console.log(e);
       self.setStatus("Error getting balance; see log.");
     });
   },
 
-  sendCoin: function() {
+  placeBet: function() {
     var self = this;
 
-    var amount = parseInt(document.getElementById("amount").value);
-    var receiver = document.getElementById("receiver").value;
+    var guess = parseInt(document.getElementById("guess").value);
+    var salt, ticketId, sealedBet;
+    var instance, roundInfo;
 
     this.setStatus("Initiating transaction... (please wait)");
 
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.sendCoin(receiver, amount, {from: account});
-    }).then(function() {
-      self.setStatus("Transaction complete!");
-      self.refreshBalance();
+    Lupi.deployed().then(function(res) {
+        instance =res;
+
+        return instance.getRoundInfo();
+    }).then( roundRes => {
+        roundInfo = self.parseRoundInfo(roundRes);
+        salt = "0x" + self.toHexString( secureRandom(32, {type: 'Array'}));
+        return instance.sealBetForAddress(account.toString(), guess, salt);
+    }).then( function(sealRes) {
+        sealedBet = sealRes;
+
+        // TODO: var callData = instance.placeBet.getData;
+        //var estimatedGas =  web3.eth.estimateGas( {from: account, to: instance.address, data: callData});
+        // console.debug("placeBet estimateGas: ", estimateGas);
+        var estimateGas = 100000;
+        return instance.placeBet(sealedBet, {from: account, value: roundInfo.requiredBetAmount, gas: estimateGas});
+    }).then(function( tx) {
+        ticketId = tx.logs[0].args.ticketId.toNumber() ;
+        self.setStatus("Successful guess. Save these to reveal your bet:"
+                + "<br> Ticket id: " + ticketId
+                + "<br> Guess: " + guess
+                + "<br> Secret key: " +  salt
+                + "<br> Account: " + account.toString());
+        self.refreshBalance();
     }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error sending coin; see log.");
+        console.log(e);
+        self.setStatus("Error sending your guess; see log.");
     });
-  }
+  },
+
+    startRevealing: function() {
+        var self = this;
+        var instance;
+
+        this.setStatus("Initiating transaction... (please wait)");
+
+        Lupi.deployed()
+        .then( res => {
+            instance = res;
+            return instance.startRevealing({from: account});
+        }).then( tx => {
+            return instance.getRoundInfo();
+        }).then( roundRes => {
+            var roundInfo = self.parseRoundInfo(roundRes);
+
+            this.setStatus("And now start to reval. Reveal period ends in " + countdown(0, roundInfo.revealPeriodEnds*1000).toString());
+        });
+    },
+
+    parseRoundInfo: function(result) {
+        return {
+            state: result[0],
+            requiredBetAmount: result[1],
+            feePt: result[2].toNumber(),
+            ticketCountLimit: result[3].toNumber(),
+            revealPeriodLength: result[4].toNumber(),
+            ticketCount: result[5].toNumber(),
+            revealedCount: result[6].toNumber(),
+            feeAmount: result[7],
+            winnablePot: result[8],
+            winningTicket: result[9].toNumber(),
+            winningAddress: result[10],
+            winningNumber: result[11].toNumber(),
+            revealPeriodEnds: result[12].toNumber()
+        }
+    },
+
+    toHexString: function(byteArray) {
+        return byteArray.map(function(byte) {
+            return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+        }).join('')
+    }
+
 };
 
 window.addEventListener('load', function() {
